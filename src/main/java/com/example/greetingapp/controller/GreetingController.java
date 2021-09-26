f1c78b1487694181ff383955158551e71a9c9cc2
Kshitija Patil <patilkshitija2@gmail.com>
2021-09-26 15:33:13 +0530

    [kshitija]:using greeting controller return json for method

---
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.greetingapp.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.greetingapp.model.Greeting;
+
+@RestController
+public class GreetingController {
+
+	private static final String template="Hello %s";
+	private final AtomicLong counter= new AtomicLong();
+	
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam (value="name",defaultValue="world") String name) {
+		
+		return new Greeting(counter.incrementAndGet(),String.format(template, name));
+		
+	}
+}
